6aedf90405c3195825f0284bfc51725f0be92171
GFT\pebo <pebo@gft.com>
2022-03-21 11:09:28 +0100

    feat: update home page style

--- src/components/yat-pages/yat-home/yat-home.tsx
+++ src/components/yat-pages/yat-home/yat-home.tsx
@@ -9,13 +9,12 @@ export class YatHome {
   render() {
     return (
       <div class="yat-home">
-        <section class="yat-home--intro section is-medium">
-          <h1 class="title">Section</h1>
-          <h2 class="subtitle">
-            A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
-          </h2>
+        <section class="yat-home--banner section is-medium">
+          <img src="./../../../assets/img/banner_adidas_02.jpg" alt="adidas-banner" />
         </section>
-        <section class="yat-home--national-teams hero is-large is-black">
+        <section class="yat-home--national-teams hero is-large">
+          <h1 class="title">Create your team</h1>
+          <h2 class="subtitle">Select players from the different national teams around the World and assign them to your own adidas team</h2>
           <yat-all-national-teams />
         </section>
       </div>
